f1fdfc9c606388a634705725049900305bad473d
Sardorbek <axadjonovsardorbeck@gmail.com>
2024-08-25 15:50:15 +0500

    cors fixed

--- service-1/api/api.go
+++ service-1/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	swaggerFiles "github.com/swaggo/files"
@@ -18,6 +19,14 @@ import (
 func NewApi(h *handler.Handler) *gin.Engine {
 	router := gin.Default()
 
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://3.68.216.185:8040"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	router.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.POST("/login", h.Login)
 
@@ -25,7 +34,7 @@ func NewApi(h *handler.Handler) *gin.Engine {
 	admin.POST("/admin/user/add", h.Register)
 	admin.GET("/admin/user/all", h.GetAllUsers)
 	admin.DELETE("/admin/user/delete", h.DeleteProfile)
-	
+
 	protected := router.Group("/", middleware.JWTMiddleware())
 	protected.GET("/profile", h.Profile)
 	protected.GET("/refresh-token", h.RefreshToken)
